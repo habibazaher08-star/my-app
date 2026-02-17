9471b04399be489699cc15ce723e34859255e92f
habibazaher08-star <habibazaher08@gmail.com>
2026-02-17 20:58:40 +0200

    updats

--- app/service/access-token.ts
+++ app/service/access-token.ts
@@ -1,11 +1,18 @@
 import { decode } from "next-auth/jwt"
 import { cookies } from "next/headers"
 
-export async function getAccessToken(){
-    const authToken = (await cookies()).get('next-auth.session-token')?.value 
-        const token = await decode({
-            token: authToken , 
-            secret: process.env.NEXTAUTH_SECRET!
-        })
-        return token?.token
+export async function getAccessToken() {
+  const cookieStore = await cookies()
+  let authToken = cookieStore.get("next-auth.session-token")?.value
+  if (!authToken) {
+    authToken = cookieStore.get("__Secure-next-auth.session-token")?.value
+  }
+  if (!authToken) return null
+
+  const token = await decode({
+    token: authToken,
+    secret: process.env.NEXTAUTH_SECRET!,
+  })
+
+  return token?.token || null
 }
