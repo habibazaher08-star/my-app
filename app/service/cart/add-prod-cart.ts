76c33200bc313b6322517f0d3eb7de85178d2060
habibazaher08-star <habibazaher08@gmail.com>
2026-02-17 21:22:13 +0200

    improvment

--- app/service/cart/add-prod-cart.ts
+++ app/service/cart/add-prod-cart.ts
@@ -1,22 +1,24 @@
 'use server'
 import { getAccessToken } from "../access-token"
 
-export async function addToCart(productId:string){
+export async function addToCart(productId: string) {
+  const token = await getAccessToken()
+  if (!token) {
+    throw new Error('Unauthorized')
+  }
 
-    const token = await getAccessToken()
-    if(!token){
-        throw new Error('Unauthorized....')
-    }
-    const response= await fetch (`${process.env.API}/cart` , {
-        method:'POST' , 
-        headers:{
-            token: token , 
-            'content-type' : 'application/json'
-        } ,
-        body:JSON.stringify({
-            productId
-        })
-    })
-   const payload= await response.json()
-   return payload
+  const response = await fetch(`https://ecommerce.routemisr.com/api/v1/cart`, {
+    method: 'POST',
+    headers: {
+      token: token,
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      productId,
+    }),
+    cache: 'no-store',
+  })
+
+  const payload = await response.json()
+  return payload
 }
--- app/service/cart/update-cart.ts
+++ app/service/cart/update-cart.ts
@@ -1,23 +1,33 @@
 'use server'
 import { getAccessToken } from "../access-token"
 
-export async function updateCartItem({productId , count}:{productId:string , count:number}){
+export async function updateCartItem({
+  productId,
+  count,
+}: {
+  productId: string
+  count: number
+}) {
+  const token = await getAccessToken()
+  if (!token) {
+    throw new Error('Unauthorized')
+  }
 
-    const token = await getAccessToken()
-    if(!token){
-        throw new Error('Unauthorized....')
+  const response = await fetch(
+    `https://ecommerce.routemisr.com/api/v1/cart/${productId}`,
+    {
+      method: 'PUT',
+      headers: {
+        token: token,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        count: count,
+      }),
+      cache: 'no-store',
     }
-    const response= await fetch (`${process.env.API}/cart/${productId}` , {
-        method:'DELETE' , 
-        headers:{
-            token: token , 
-            'content-type' : 'application/json'
-        } ,
-        body:JSON.stringify({
-            count:count
-        })
-    })
-   const payload= await response.json()
-   return payload
+  )
+
+  const payload = await response.json()
+  return payload
 }
-  
