15fa9112d95fca4787806b45fff1b710bb671d81
habibazaher08-star <habibazaher08@gmail.com>
2026-02-17 22:11:08 +0200

    uppppdateeee

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -6,7 +6,8 @@ export async function GET(req:NextRequest){ //m3aya el req
 
  const token =  await getToken({req}) //elmethod eli btgeble el token
  if(!token){
-    return NextResponse.json({error:'Unauthorized' , satus:401}) //da eli ha y7sal lw msh m3ah token
+    return NextResponse.json({ error: "Unauthorized" },
+      { status: 401 } ) //da eli ha y7sal lw msh m3ah token
  }
  const response = await fetch(`${process.env.API}/cart` , { //fe halet en m3ah token // bageb el api
     headers:{ 
